3ae8653a5c9d7a8268358ed946c830f063edb37e
paresh05 <pareshpraveen.surathkal@gmail.com>
2022-01-21 11:59:10 +0530

    Created: A react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="topbar">
+      <img
+        className="logo"
+        src="https://b.zmtcdn.com/web_assets/8313a97515fcb0447d2d77c276532a511583262271.png"
+      ></img>
+      <h1 className="title">Discover the best food & drinks in Bengaluru</h1>
+    </div>
+  );
+}
+
+export default App;
